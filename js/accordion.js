7a043d0fb4f307dc8e7d0f27d20254993d4f5770
web-dew-sat <78866552+web-dew-sat@users.noreply.github.com>
2021-10-29 02:16:36 +0300

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,32 @@
+// const lists = document.querySelectorAll('.feature-sub')
+// const btns = document.querySelectorAll('.feature__link')
+
+// btns.forEach((btnItem, index) => {
+//     btnItem.addEventListener('click', () => {
+//         btns.forEach((btnItem) => {
+//             btnItem.classList.remove('feature__link_active')
+//         })
+//         btnItem.classList.add('feature__link_active')
+
+//         lists.forEach((listItem) => {
+//             listItem.classList.add('hidden')
+//         })
+//        lists[index].classList.remove('hidden')
+//     })
+    
+// })
+
+
+
+
+const lists = document.querySelectorAll('.feature-sub')
+const btns = document.querySelectorAll('.feature__link')
+
+btns.forEach((btnItem, index) => {
+    btnItem.addEventListener('click', () => {
+       
+        btnItem.classList.toggle('feature__link_active')
+        lists[index].classList.toggle('hidden')
+    })
+    
+})
