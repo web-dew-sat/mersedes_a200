eb1dadbc0413f32b815347597734f0c8fdfa3da4
web-dew-sat <78866552+web-dew-sat@users.noreply.github.com>
2021-10-29 03:31:57 +0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,22 @@
+const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]')
+const tabsContentElems = document.querySelectorAll('[data-tabs-field]')
+
+
+for(let btn of tabsHandlerElems) {
+    btn.addEventListener('click', () => {
+        tabsHandlerElems.forEach(item => {
+            item.classList.remove('design-list__item_active')
+        })
+        btn.classList.add('design-list__item_active')
+
+        tabsContentElems.forEach(cont => {
+            if(cont.dataset.tabsField === btn.dataset.tabsHandler) {
+                cont.classList.remove('hidden')
+            } else {
+                cont.classList.add('hidden')
+            }
+        })
+    })
+}
+
+
